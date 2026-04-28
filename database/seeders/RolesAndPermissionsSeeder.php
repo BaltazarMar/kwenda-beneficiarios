93841cbe3c84d00ae9aabfffaba2ab7a9ebfc36a
Baltazar Mariano <baltazarmarianobm@gmail.com>
2026-04-28 14:18:45 +0100

    Corrigir seeder roles

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -2,16 +2,21 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Roles
+        Role::firstOrCreate(['name' => 'director',        'guard_name' => 'web']);
+        Role::firstOrCreate(['name' => 'assistente_dados','guard_name' => 'web']);
+        Role::firstOrCreate(['name' => 'assistente_local','guard_name' => 'web']);
+        Role::firstOrCreate(['name' => 'recepcionista',   'guard_name' => 'web']);
+        Role::firstOrCreate(['name' => 'motorista',       'guard_name' => 'web']);
     }
 }
